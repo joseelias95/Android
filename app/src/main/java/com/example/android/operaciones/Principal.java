290cfa8b52b378d80492b5e5cadb912e63c0cc82
Jose elias <jbarraza@fundacionidi.org>
2017-08-22 20:04:36 -0500

    Interfaz grafica terminada

--- app/src/main/java/com/example/android/operaciones/Principal.java
+++ app/src/main/java/com/example/android/operaciones/Principal.java
@@ -4,13 +4,17 @@ import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Principal extends AppCompatActivity {
     private TextView res;
     private EditText n1, n2;
     private Resources resources;
+    private Spinner operaciones;
+    private String op [];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,14 @@ public class Principal extends AppCompatActivity {
         res = (TextView)findViewById(R.id.lblResultado);
         n1 = (EditText) findViewById(R.id.txtNumerouno);
         n2 = (EditText) findViewById(R.id.txtNumerodos);
+        operaciones = (Spinner) findViewById(R.id.cmbOperaciones);
+
+
         resources = this.getResources();
+        op = resources.getStringArray(R.array.operaciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, op);
 
-
-
-
+        operaciones.setAdapter(adapter);
     }
 
     public void calcular(View v){
